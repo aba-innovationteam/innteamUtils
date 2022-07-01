fd4f4785a61bf7e887e17b10f103232e81beceb2
Alejandro <a.abraham@innovationteam.eu>
2022-07-01 09:22:13 +0100

    new logo

--- inst/logo/logo.R
+++ inst/logo/logo.R
@@ -1,16 +1,16 @@
 
-library("magick")
-library("showtext")
+#library("magick")
+#library("showtext")
 
-font_add_google("Open Sans", "pf", regular.wt = 300)
+#font_add_google("Open Sans", "pf", regular.wt = 300)
 
-hexSticker::sticker(
-  subplot = ~ plot.new(), s_x = 1, s_y = 1, s_width = 0.1, s_height = 0.1,
-  package = "INNTEAMUTILS", p_x = 1, p_y = 1, p_size = 14, h_size = 1.2, p_family = "pf",
-  p_color = "#FFFFFF", h_fill = "#037F8C", h_color = "#225E64",
-  dpi = 320, filename = "man/figures/logo.png"
-)
+#hexSticker::sticker(
+#  subplot = ~ plot.new(), s_x = 1, s_y = 1, s_width = 0.1, s_height = 0.1,
+#  package = "INNTEAMUTILS", p_x = 1, p_y = 1, p_size = 14, h_size = 1.2, p_family = "pf",
+#  p_color = "#FFFFFF", h_fill = "#037F8C", h_color = "#225E64",
+#  dpi = 320, filename = "man/figures/logo.png"
+#)
 
-image_read("man/figures/logo.png")
+#image_read("man/figures/logo.png")
 
-rstudioapi::restartSession()
+#rstudioapi::restartSession()
